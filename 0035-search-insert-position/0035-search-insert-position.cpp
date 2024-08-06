a30fc930a2feb5c53ce3041675f5571e117cc630
omin23 <114853674+omin23@users.noreply.github.com>
2024-08-06 17:29:35 +0530

    Time: 3 ms (71.38%), Space: 12.4 MB (26.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       auto res = lower_bound(nums.begin(),nums.end(),target);
+        return res - nums.begin();
+    }
+};
