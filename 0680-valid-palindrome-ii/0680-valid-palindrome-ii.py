5c6a425586b10a082726164388d30fcc6ad41a7e
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-02 10:57:48 +0530

    Time: 56 ms (66.51%), Space: 18.1 MB (14.29%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,16 +1,6 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def validP(s:str):
-            l,r = 0, len(s)-1
-            counter = 0
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l+=1
-                r-=1
-            return True
-        
-        
+                
         l,r = 0, len(s)-1
         counter = 0
 
@@ -20,9 +10,9 @@ class Solution:
                 if counter > 1:
                     return False
                 if s[l] == s[r-1] and s[l+1] == s[r]:
-                    Sone = validP(s[l:r])
-                    Stwo = validP(s[(l+1):(r+1)])
-                    return Sone or Stwo
+                    skipL = s[l:r]
+                    skipR = s[(l+1):(r+1)]
+                    return skipL == skipL[::-1] or skipR == skipR[::-1]
                 elif s[l] == s[r-1]:
                     r-=1
                 elif s[l+1] == s[r]:
