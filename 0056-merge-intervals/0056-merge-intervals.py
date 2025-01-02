baabaac7f29060ef8cf90caa9cb64fa39324f35f
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-02 18:08:33 +0700

    Time: 16 ms (7.58%), Space: 20.8 MB (44.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -8,9 +8,8 @@ class Solution:
             # print(i)
             if i < len(intervals) - 1 and intervals[i][1] >= intervals[i+1][0]:
                 # print(i,n)
-                intervals[i+1][0] = intervals[i][0] if intervals[i+1][0] > intervals[i][0] else intervals[i+1][0]
-                intervals[i+1][1] = intervals[i+1][1] if intervals[i+1][1] > intervals[i][1] else intervals[i][1]
-                intervals.pop(i)
+                intervals[i][1] = max(intervals[i+1][1], intervals[i][1])
+                intervals.pop(i+1)
                 n+=1
             # print(intervals)
         return intervals
