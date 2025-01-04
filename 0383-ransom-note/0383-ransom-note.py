78cd137fd751fb35fd52a3ad8ddd24c5b0037d41
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-04 12:29:54 +0700

    Time: 13 ms (81.52%), Space: 17.6 MB (38.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,19 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        maglib = defaultdict(int)
-        for i in magazine:
-            maglib[i] += 1 
-        for i in (ransomNote):
-            if i not in maglib:
-                return False 
-            maglib[i] -= 1 
-            if maglib[i] == -1:
+        # maglib = defaultdict(int)
+        # for i in magazine:
+        #     maglib[i] += 1 
+        # for i in (ransomNote):
+        #     if i not in maglib:
+        #         return False 
+        #     maglib[i] -= 1 
+        #     if maglib[i] == -1:
+        #         return False
+        # return True 
+        countmag = Counter(magazine)
+        countran = Counter(ransomNote)
+        for i in countran:
+            if countran[i] > countmag[i]:
                 return False
-        return True 
-        
+        return True
