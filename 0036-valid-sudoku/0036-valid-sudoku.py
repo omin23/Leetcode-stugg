cd9e34a6c4a5210033ab460b68b87a702b496fc6
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-04 17:40:44 +0530

    Time: 6 ms (45.04%), Space: 18 MB (7.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,9 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         #each row 
+        check = set()
         for i in range(9):
-            check = set()
+            check.clear()
             for j in range(9):
                 if board[i][j] in check:
                     return False
@@ -11,7 +12,7 @@ class Solution:
 
         # #each col
         for i in range(9):
-            check = set()
+            check.clear()
             for j in range(9):
                 if board[j][i] in check:
                     return False
@@ -21,7 +22,7 @@ class Solution:
         #each 3 by 3
         for col in range(3):
             for row in range(3):
-                check = set()
+                check.clear()
                 for i in range(3):
                     for j in range(3):
                         if board[(3*col) + i][(3*row) + j] in check:
