78f44ad48c7de1cc65ccd6c4b83bfe55510d5820
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-05 09:47:36 +0530

    Time: 3 ms (81.59%), Space: 18.5 MB (18.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,11 +3,11 @@ class Solution:
         lf,ri = 0,len(numbers)-1
         while ri > lf:
             sum = numbers[lf] + numbers[ri]
-            
+            if sum == target:
+                return[lf+1,ri+1]
             if sum > target:
                 ri -=1
-            elif sum < target:
-                lf += 1
             else:
-                return[lf+1,ri+1]
-        return
+                lf += 1
+
+        return[lf,ri+1]
