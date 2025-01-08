cf218ab4013ab270ab7bafc4147ca348e513d170
omin23 <114853674+omin23@users.noreply.github.com>
2025-01-08 18:57:09 +0530

    Time: 45 ms (97.16%), Space: 59.4 MB (44.77%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         nums = [i for i in range(1,n+1)]
-        print(nums)
-        sol, res = [],[]
-        def bt(i,j):
-            if i == k:
-                res.append(sol[:])
-                return
+        res = combinations(nums,k)
+        final = [list(i) for i in res]
+        return final 
 
-            for j in range(j,n):
-                sol.append(nums[j])
-                bt(i+1,j+1)
-                sol.pop()
-            return       
+        # sol, res = [],[]
+        # def bt(j):
+        #     if len(sol) == k:
+        #         res.append(sol[:])
+        #         return
+
+        #     for j in range(j,n):
+        #         sol.append(nums[j])
+        #         bt(j+1)
+        #         sol.pop()
+        #     return       
         
-        bt(0,0)
+        # bt(0)
         return res 
