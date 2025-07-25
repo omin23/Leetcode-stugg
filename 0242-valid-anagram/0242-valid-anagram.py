8cf293985516941c9500cb5f9996c1fee31bbafd
omin23 <114853674+omin23@users.noreply.github.com>
2025-07-25 03:24:43 -0400

    Time: 24 ms (39.24%), Space: 12.6 MB (68.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,13 @@
 from collections import Counter
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s):
-                return False
-
-        cs = Counter(s)
-        ct = Counter(t)
-        if cs != ct:
-            return False 
-        return True 
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if Counter(s) == Counter(t):
+            return True
+        return False 
+        
