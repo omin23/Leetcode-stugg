5a0889879e92c552c342178a3842a910d9555ced
omin23 <114853674+omin23@users.noreply.github.com>
2025-07-25 03:50:12 -0400

    Time: 10 ms (87.14%), Space: 21.3 MB (40.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,11 +2,10 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         word_mapper = defaultdict(list)
+        res = []
         
         for i in strs:
             word_mapper[tuple(sorted(i))].append(i)
-        
-        res = []
         for i in word_mapper.values():
             res.append(i)
 
