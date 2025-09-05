13d63a00afe031fe9a9e6f3ad748b524a5b7fcff
omin23 <114853674+omin23@users.noreply.github.com>
2025-09-04 23:42:09 -0400

    Time: 24 ms (53.67%), Space: 23.2 MB (93.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,38 +1,40 @@
+from collections import Counter
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # s = Counter(nums)
         length = len(nums)
-        res = [1] * length
-        # print(res)
-        num_zeros = 0
-        mapped = Counter(nums)
-
-        # zero_loc = 0
-        # for i in range(length):
-        #     if nums[i] == 0:
-        #         zero_loc = i 
-        #         num_zeros += 1
-        
-        if mapped[0] > 1:
-            return [0] * length
-        
-        # if num_zeros == 1:
-        #     total = 1
-        #     for i in nums:
-        #         if i != 0:
-        #             total *= i
-        #     res[zero_loc] = total
+        # res = [0] * length
+        # if s[0] > 1:
         #     return res
-
+        # elif s[0] == 1:
+        #     total = 1
+        #     for i in range(length):
+        #         if nums[i] != 0:
+        #             total *= nums[i]
+        #         else:
+        #             idz = i
+        #     res[idz] = total
+        #     return res
+        # # no 0's
+        res = [1] * length
         for i in range(1,length):
-            res[i] = res[i-1] * nums[i-1]
-        # print(res)
-        suffix = 1 
-        for i in range(length-1,0,-1):
-            # print(i)
-            suffix *= nums[i]
-            res[i-1] *= suffix
-        # print(res)
+            res[i] *= nums[i-1] * res[i-1]
+        runner = 1
+        for i in range(length-2,-1,-1):
+            runner *= nums[i+1]
+            res[i] *= runner
         return res
+        
 
 
+
+
+
+
+
+
+
+
+
+        
         
