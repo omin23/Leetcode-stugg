c6ecde859f9a53a622133c91383399572d35f1d3
omin23 <114853674+omin23@users.noreply.github.com>
2025-09-11 17:48:06 -0400

    Time: 803 ms (5.12%), Space: 74 MB (11.01%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.py
@@ -0,0 +1,41 @@
+from collections import defaultdict 
+
+class FindSumPairs:
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.l1 = nums1
+        self.l2 = nums2
+        self.l2set = defaultdict(list)
+        for i in enumerate(nums2):
+            pos,val = i 
+            self.l2set[val].append(pos)
+        
+
+
+    def add(self, index: int, val: int) -> None:
+        old = self.l2[index]
+        self.l2[index] += val
+        self.l2set[old].remove(index)
+        self.l2set[self.l2[index]].append(index)
+
+
+
+    def count(self, tot: int) -> int:
+        ret = 0 
+        usearr = self.l1
+        for i in usearr:
+            l2maybe = tot-i
+            if l2maybe in self.l2set.keys(): ret += len(self.l2set[l2maybe])
+        
+        # print(ret)
+        return ret
+
+
+        
+
+        
+
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
