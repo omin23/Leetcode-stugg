fa665b65b0ce3aeb6a690a00c31d09762f9a74ab
omin23 <114853674+omin23@users.noreply.github.com>
2025-09-12 11:38:01 -0400

    Time: 0 ms (100%), Space: 18.1 MB (18.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,40 +1,102 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        sc = 0
-        ypos = 0 
-        xpos = 0
-        ymax = len(matrix)-1
-        xmax = len(matrix[0])-1
-        total = (ymax+1)*(xmax+1)
+        l,r = -1,len(matrix[0])
+        b,t = 0,len(matrix)
+        done = r * t
+
         res = []
-        res.append(matrix[ypos][xpos])
-        while len(res) != total:
-            # go accross to the right 
-            while xpos < xmax-sc:
-                xpos +=1
-                res.append(matrix[ypos][xpos])
-                if len(res) == total:
-                    return res 
+        x,y = 0,0
+        print("top:",t,"bottom: ",b,"left: ",l,"right: ",r)
+        spiral = False 
+        while True: 
+            # go right 
+            print("spiral")
+            if spiral:
+                x +=1
+            while x != r:
+                print(x,y)
+                res.append(matrix[y][x])
+                x += 1
+            x -= 1
+            r -=1
+            if len(res) == done:
+                return res 
             # go down 
-            while ypos < ymax-sc:
-                ypos += 1
-                res.append(matrix[ypos][xpos])
-                if len(res) == total:
-                    return res 
-            # go to the left 
-            while xpos > 0+sc:
-                xpos -=1
-                res.append(matrix[ypos][xpos])
-                if len(res) == total:
-                    return res 
-            # go up 
-            while ypos > 1+sc:
-                ypos -=1
-                res.append(matrix[ypos][xpos])
-                if len(res) == total:
-                    return res 
-            sc+=1
-        return res 
+            y += 1
+            while y != t:
+                print(x,y,t)
+                res.append(matrix[y][x])
+                y +=1
+            y -=1
+            t -=1
+            if len(res) == done:
+                return res 
+            
+            # left 
+            x -=1
+            while x != l:
+                print(x,y)
+                res.append(matrix[y][x])
+                x -=1
+            x +=1
+            l +=1
+            if len(res) == done:
+                return res 
+            
+            # up 
+            y -=1
+            while y != b:
+                print(x,y)
+                res.append(matrix[y][x])
+                y -=1
+            y +=1
+            b +=1
+            if len(res) == done:
+                return res 
+            spiral = True
 
 
-        
+
+            # # go right
+            # for j in range(width): 
+            #     x = j 
+            #     res.append(matrix[y][x])
+            # width -= 1 
+            # height -= 1
+            # if len(res) == done:
+            #     return res
+            # print(res,x,y)
+            
+            # # go down 
+            # for j in range(1,height):
+            #     y = j
+            #     print(x,y)
+            #     res.append(matrix[y][x])
+            # height -= 1
+            # width -= 1 
+            # if len(res) == done:
+            #     return res
+            # print(res,x,y)
+
+            # # go left 
+            # for j in range(width):
+            #     x -= 1
+            #     res.append(matrix[y][x])
+            # width -= 1 
+            # if len(res) == done:
+            #     return res
+            # height -= 1
+            # width -= 1 
+            # print(res,x,y)
+            
+            # # go up 
+            # for j in range(1,height):
+            #     y -= 1
+            #     res.append(matrix[y][x])
+            # height -= 1
+            # width -= 1 
+            # if len(res) == done:
+            #     return res
+            # print(res,x,y)
+
+        return ["huh"]
