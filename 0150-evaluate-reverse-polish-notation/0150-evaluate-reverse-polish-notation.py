3d7e57625917ffa4b3265f0d47a939807acb4b79
omin23 <114853674+omin23@users.noreply.github.com>
2025-10-01 09:24:10 -0400

    Time: 0 ms (100%), Space: 19.3 MB (16.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,28 +1,33 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        n = []
-
-        for i in tokens:
-            if i == "+":
-                n1 = n.pop()
-                n2 = n.pop()
-                n.append(n1 + n2)
-            elif i == "-":
-                n1 = n.pop()
-                n2 = n.pop()
-                n.append(n2 - n1)
-            elif i == "*":
-                n1 = n.pop()
-                n2 = n.pop()
-                n.append(n1 *n2)
-            elif i == "/":
-                n1 = n.pop()
-                n2 = n.pop()
-                n.append(int(n2/n1))
+        if len(tokens) < 2: return int(tokens[0])
+        nums = []
+        for i in range(len(tokens)): 
+            if tokens[i].isdigit() or tokens[i][1:].isdigit():
+                nums.append(tokens[i])
             else:
-                n.append(int(i))
-        return n[-1]
-        
+                # print(nums)
+                num1 = int(nums.pop())
+                num2 = int(nums.pop())
+                op = tokens[i]
+                # print(op)
+                if op == "+": 
+                    # print(num2,op,num1)
+                    nums.append(num1 + num2)
+                elif op == "-":
+                    # print(num2,op,num1)
+                    nums.append(num2 - num1)
+                elif op == "*": 
+                    # print(num2,op,num1)
+                    nums.append(num1 * num2)
+                else: 
+                    # print(num2,op,num1)
+                    nums.append(int(num2 / num1))  
 
+        # for i in opers: 
+        #     print(nums, opers)
+        #     num1 = int(nums.pop())
+        #     num2 = int(nums.pop())
+        #     op = opers.pop()
             
-        
+        return nums[0]
