862c4ec1553ee7a2d8d098941fb4372e901de8bd
omin23 <114853674+omin23@users.noreply.github.com>
2025-10-22 00:02:28 -0400

    Time: 152 ms (71.29%), Space: 18.9 MB (94.23%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def check(i,h,piles): return sum([((j+i-1)//i) for j in piles]) <= h
+        maxnum = max(piles)
+        l,r = 1,maxnum
+        worked = maxnum
+        while l <= r: 
+            m = (l+r)//2
+            # print(m)
+            if check(m,h,piles):
+                worked = min(worked,m)
+                r = m - 1
+            else: 
+                l = m + 1
+        if check(m,h,piles): return m
+        return worked
+
+
+
+        # new = sorted(piles)
