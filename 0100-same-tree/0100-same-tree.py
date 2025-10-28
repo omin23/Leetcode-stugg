909d0fba2bb7ba74cdfdb040c7628422323d4db7
omin23 <114853674+omin23@users.noreply.github.com>
2025-10-28 01:30:40 -0400

    Time: 0 ms (100%), Space: 18 MB (21.2%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,10 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (not p and q) or (not q and p): return False
-        def inorder(root1,root2):
-            if not root1 and not root2: return True
-            elif (not root1 and root2) or (not root2 and root1) or (root1.val != root2.val) : return False
-            return inorder(root1.right,root2.right) and inorder(root1.left,root2.left)
-        return inorder(p,q)
+        good = [True]
+        def order(p,q):
+            if (not p and q) or (not q and p) :good[0] = False
+            if not p and not q: return 
+            if not good[0]: return False
+            if p.val == q.val:
+                order(p.right,q.right)
+                order(p.left,q.left)
+            else: good[0] = False
+        order(p,q)
+        return good[0] 
+
         
