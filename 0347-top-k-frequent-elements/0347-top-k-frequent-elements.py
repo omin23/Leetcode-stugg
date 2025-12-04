d2588ca6a2ce4ce7e00b46be3416606a620c44b1
omin23 <114853674+omin23@users.noreply.github.com>
2025-12-04 12:15:00 -0500

    Time: 3 ms (90.47%), Space: 21.4 MB (28.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,13 @@
-from collections import Counter
-# import heapq
+from collections import defaultdict, Counter 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-       freqk = Counter(nums)
-       items = sorted(freqk.items(), key = lambda x: x[1],reverse=True)
-       return [items[i][0] for i in range(k)]
-
+        # mapper = Defaultdict(int)
+        # mapper = {}
+        # for i in nums: 
+        #     mapper[i] = mapper.get(i,0) + 1
+        mapper = Counter(nums)
+        new = (sorted(mapper.items(),key=lambda mappe: mappe[1], reverse =True))
+        res = [x[0] for x in new][:k]
+        return res
 
 
