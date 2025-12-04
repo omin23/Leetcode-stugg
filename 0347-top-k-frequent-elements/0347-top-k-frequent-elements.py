1f555dc36a62a8f38231ab1b5952eeaa4cc675ba
omin23 <114853674+omin23@users.noreply.github.com>
2025-12-04 12:15:25 -0500

    Time: 0 ms (100%), Space: 21.2 MB (80.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,11 +2,11 @@ from collections import defaultdict, Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # mapper = Defaultdict(int)
-        # mapper = {}
-        # for i in nums: 
-        #     mapper[i] = mapper.get(i,0) + 1
-        mapper = Counter(nums)
-        new = (sorted(mapper.items(),key=lambda mappe: mappe[1], reverse =True))
+        mapper = {}
+        for i in nums: 
+            mapper[i] = mapper.get(i,0) + 1
+        # mapper = Counter(nums)
+        new = (sorted(mapper.items(),key=lambda mappe: mappe[1], reverse=True))
         res = [x[0] for x in new][:k]
         return res
 
