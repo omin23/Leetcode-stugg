5b63a39e818152a8f927988c3b2adabefda85297
omin23 <114853674+omin23@users.noreply.github.com>
2026-01-01 01:10:33 -0500

    Time: 50 ms (7.49%), Space: 24.7 MB (36.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,40 +1,84 @@
-from collections import Counter
+from collections import Counter 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # s = Counter(nums)
         length = len(nums)
-        # res = [0] * length
-        # if s[0] > 1:
-        #     return res
-        # elif s[0] == 1:
-        #     total = 1
-        #     for i in range(length):
-        #         if nums[i] != 0:
-        #             total *= nums[i]
-        #         else:
-        #             idz = i
-        #     res[idz] = total
-        #     return res
-        # # no 0's
-        res = [1] * length
+        oc = Counter(nums)
+        if oc[0] > 1:
+            res = [0 for i in range(length)]
+            return res
+        elif oc[0] == 1: 
+            res = [0 for i in range(length)]
+            num = 1
+            for i in nums: 
+                if i != 0: 
+                    num *= i
+            res[nums.index(0)] = num
+            return res
+
+        pre = [1 for i in range(length)]
         for i in range(1,length):
-            res[i] *= nums[i-1] * res[i-1]
-        runner = 1
+            pre[i] = pre[i-1]*nums[i-1]
+        post = [1 for i in range(length)]
         for i in range(length-2,-1,-1):
-            runner *= nums[i+1]
-            res[i] *= runner
-        return res
+            post[i] = post[i+1]*nums[i+1]
+        res = [pre[i]*post[i] for i in range(length)]
+        return res 
+
+
+
+
+
+
+
+
+            
+
+    
+    
         
-
-
-
-
-
-
-
-
-
-
-
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # length = len(nums)
+        # res = [1] * length
+        # # print(res)
+        # num_zeros = 0
+        # mapped = Counter(nums)
+
+        # # zero_loc = 0
+        # # for i in range(length):
+        # #     if nums[i] == 0:
+        # #         zero_loc = i 
+        # #         num_zeros += 1
+        
+        # if mapped[0] > 1:
+        #     return [0] * length
+        
+        # # if num_zeros == 1:
+        # #     total = 1
+        # #     for i in nums:
+        # #         if i != 0:
+        # #             total *= i
+        # #     res[zero_loc] = total
+        # #     return res
+
+        # for i in range(1,length):
+        #     res[i] = res[i-1] * nums[i-1]
+        # # print(res)
+        # suffix = 1 
+        # for i in range(length-1,0,-1):
+        #     # print(i)
+        #     suffix *= nums[i]
+        #     res[i-1] *= suffix
+        # # print(res)
+        # return res
+
+
         
