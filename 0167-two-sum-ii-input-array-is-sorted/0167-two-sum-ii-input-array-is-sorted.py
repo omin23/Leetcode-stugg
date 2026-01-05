7136cc38c2ae74185fe1fc9c4db55dd1329ca55f
omin23 <114853674+omin23@users.noreply.github.com>
2026-01-05 11:46:37 -0500

    Time: 5 ms (34.55%), Space: 18.4 MB (96.13%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,32 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-       l,r = 0, len(numbers)-1
-       curr = 0 
-       while l < r:
-        curr = numbers[l] + numbers[r]
-        if curr == target:
-            return [l+1,r+1]
-        elif curr < target: 
-            l+=1
-            while numbers[l] == numbers[l-1]:
-                 l+=1
-        else: 
-            r-=1
-            while numbers[r] == numbers[r+1]:
-                 r-=1
+        right = len(numbers) - 1
+        left = 0 
+        res = numbers[right] + numbers[left]
+        while res != target:
+            if res > target: 
+                right -=1
+            elif res < target: 
+                left +=1
+            res = numbers[right] + numbers[left]
+        return[left+1,right+1]
+
+
+
+
+
+
+
+        
+        
+        
+        # right_pointer = len(numbers)-1
+        # left_pointer = 0 
+        # while right_pointer > left_pointer: 
+        #     if numbers[right_pointer] + numbers[left_pointer] > target:
+        #         right_pointer -= 1
+        #     elif numbers[right_pointer] + numbers[left_pointer] < target:
+        #         left_pointer += 1
+        #     else:
+        #         break
+        # return [left_pointer+1,right_pointer+1]
