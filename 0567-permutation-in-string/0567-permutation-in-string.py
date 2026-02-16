3aa6f5616a4b527ddd271936e6edd6b1f7c6535f
omin23 <114853674+omin23@users.noreply.github.com>
2026-02-16 14:15:40 -0500

    Time: 23 ms (48.2%), Space: 19.5 MB (39.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,13 +4,12 @@ class Solution:
         checker = Counter(s1)
         needed = len(checker)
         ws = len(s1)
-
         for i in range(len(s2)):
             checker[s2[i]] -= 1
             if checker[s2[i]] == 0: needed -=1
             if i >= ws: 
                 checker[s2[i-ws]] += 1
-                if checker[s2[i-ws]] == 1:needed +=1
+                if checker[s2[i-ws]] == 1: needed +=1
             if needed == 0: return True 
         return False 
 
